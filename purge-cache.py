c968188d2ed046a1ad0bf41a332974958fd09eca
Isaac <undermybrella@abimon.org>
2019-01-31 15:06:21 +1100

    Add purge-cache.py

---
+++ purge-cache.py
@@ -0,0 +1,27 @@
+import CloudFlare
+import os
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def main():
+    cf = CloudFlare.CloudFlare()
+    paths = []
+    for subdir, dirs, files in os.walk("./"):
+        if (subdir == "./"):
+            for file in files:
+                paths.append(subdir.replace("./", "https://spiralframework.info") + "/" + file)
+        else:
+            for file in files:
+                paths.append(subdir.replace("./", "https://spiralframework.info/") + "/" + file)
+    for list in chunks(paths, 30):
+        try:
+            cf.zones.purge_cache.post("391427c4a21b8d2008a8b05f92ad4329", data = {'files': list})
+            print('purged ' + str(len(list)) + ' paths')
+        except CloudFlare.exceptions.CloudFlareAPIError as e:
+            exit('purge cache - %d %s - api call failed' % (e, e))
+
+if __name__ == '__main__':
+    main()
